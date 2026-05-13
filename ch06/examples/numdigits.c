903e2976d8a307f2925052804d51d902bfa50827
PlexusDeus <plexuscoroideus@outlook.com>
2026-05-12 20:17:20 -0600

    ch06 examples

---
+++ ch06/examples/numdigits.c
@@ -0,0 +1,18 @@
+/* numdigits.c (Chapter 6, page 105) */
+/* Calculates the number of digits in an integer */
+
+#include <stdio.h>
+
+int main(void) {
+  int digits = 0, n;
+
+  printf("Enter a nonnegative integer: ");
+  scanf("%d", &n);
+
+  do {
+    n /= 10;
+    digits++;
+  } while (n > 0);
+
+  printf("The number has %d digit(s).\n", digits);
+}
